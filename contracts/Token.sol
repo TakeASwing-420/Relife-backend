0421ac09c67d531188cb3d2ca3d5cf95715491ff
Deep Mondal <139104266+TakeASwing-420@users.noreply.github.com>
2024-05-29 22:24:55 +0530

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,14 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./database_helper.sol";
 
-contract Hikiko is ERC20Capped, Ownable, UserDB, ERC20Burnable{ 
+contract Hikiko is ERC20Capped, Ownable, UserDB, ERC20Burnable, ReentrancyGuard {
     address public Manager;
     
     uint256 public limit = 2000000000 ;
@@ -17,24 +19,24 @@ contract Hikiko is ERC20Capped, Ownable, UserDB, ERC20Burnable{
         _mint(Manager, initial_coinbase);
     }
 
-      function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
+    function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
         ERC20Capped._mint(account, amount);
     }
 
     // Allow users to transfer tokens to the Manager when redeeming rewards
-    function redeemTokens(uint256 amount, string calldata name) public onlyOwner {
-        address x= wallets[name].wallet;
+    function redeemTokens(uint256 amount, string calldata name) public onlyOwner nonReentrant {
+        address x = wallets[name].wallet;
         _transfer(x, Manager, amount);
     }
 
     // Allow the user to fetch tokens from the contract
-    function fetchTokens(uint256 amount,string calldata name) public onlyOwner{
-        address x= wallets[name].wallet;
+    function fetchTokens(uint256 amount, string calldata name) public onlyOwner nonReentrant {
+        address x = wallets[name].wallet;
         transfer(x, amount);
     }
 
-    function delete_user(string calldata name) public onlyOwner{
-        address x= wallets[name].wallet;
+    function delete_user(string calldata name) public onlyOwner nonReentrant {
+        address x = wallets[name].wallet;
         uint256 amount = balanceOf(x);
         _transfer(x, Manager, amount); // Transfer all the amount of tokens before deletion
         delete wallets[name];
@@ -48,21 +50,20 @@ contract Hikiko is ERC20Capped, Ownable, UserDB, ERC20Burnable{
     // Function to mint coins to the manager
     function additionalcoins() private {
         uint256 a;
-        if(shouldMintCoins()==false){ 
-            a=0;
+        if (shouldMintCoins() == false) { 
+            a = 0;
+        } else {
+            a = (initial_coinbase / 2) - balanceOf(Manager);
+            _mint(Manager, a);
         }
-        else{
-        a = (initial_coinbase / 2) - balanceOf(Manager);
-        _mint(Manager,a);
-              }
     }
 
-    //Update the wallet address
-    function Update_address(string calldata name, address data) public onlyOwner{
-        address x= wallets[name].wallet;
+    // Update the wallet address
+    function Update_address(string calldata name, address data) public onlyOwner nonReentrant {
+        address x = wallets[name].wallet;
         uint256 amount = balanceOf(x);
-        _transfer(x, data, amount);//Before doing so send the existing tokens to new account
-         wallets[name].wallet = data;
+        _transfer(x, data, amount); // Before doing so send the existing tokens to new account
+        wallets[name].wallet = data;
     }
     
     function saveUser(address wallet, string memory name, string memory password) public onlyOwner {
@@ -79,9 +80,7 @@ contract Hikiko is ERC20Capped, Ownable, UserDB, ERC20Burnable{
         wallets[name].cid = data;
     }
     
-    function setChallenges(string calldata name, uint256 challengeIndex, bool isActive) public onlyOwner{
-    wallets[name].challenges[challengeIndex] = isActive;
-}
-
-
+    function setChallenges(string calldata name, uint256 challengeIndex, bool isActive) public onlyOwner {
+        wallets[name].challenges[challengeIndex] = isActive;
+    }
 }
